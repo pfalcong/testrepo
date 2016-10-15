c026cb804bdd6dff5f4710e9a233021a95c0e989
Pablo Falcon <pablo.falcon@gmail.com>
2016-10-15 19:56:21 +0100

    Add static method

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,9 @@ public class HelloWorld {
 				public static void main(String[] args) {
 					System.out.println("Hello World");
 				}
+
+				public static int getNumber() {
+					return 10;	
+				}
 }
 
