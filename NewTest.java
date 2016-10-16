0d30ff04e55da1e0a303ddd1f3b2456c7f29a924
Pablo Falcon <pablo.falcon@gmail.com>
2016-10-16 12:26:51 +0100

    a difference

--- NewTest.java
+++ NewTest.java
@@ -1,6 +1,6 @@
 public class NewTest {
   public static void main(String[] args) {
-	  System.out.println("New Test");
+	  System.out.println("New Test with a difference");
 	}
 
 	public static void doNothing() {}
