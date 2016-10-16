0102f024995164d07fff4aa4c30b330c0cab7404
Pablo Falcon <pablo.falcon@gmail.com>
2016-10-16 12:26:22 +0100

    poxy difference

--- NewTest.java
+++ NewTest.java
@@ -1,6 +1,6 @@
 public class NewTest {
   public static void main(String[] args) {
-	  System.out.println("New Test");
+	  System.out.println("New Test with some other poxy difference");
 	}
 
 	public static void doNothing() {}
